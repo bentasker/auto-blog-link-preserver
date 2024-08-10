a65834a793917a60787ce9d16ffd9f1efffaa3b8
B Tasker <github@bentasker.co.uk>
2024-08-10 12:28:36 +0100

    chore: refactor out the mastodon specific stuff
    This leaves us with a body of code which
    
    * fetches a rss feed
    * iterates through unseen entries
    
    But doesn't do anything interesting with them

--- app/main.py
+++ app/main.py
@@ -7,86 +7,6 @@ import requests
 import time
 
 
-def build_toot(entry):
-    ''' Take the entry dict and build a toot
-    '''
-    
-    skip_tags = SKIP_TAGS
-    skip_tags.append("blog")
-    skip_tags.append("documentation")
-    skip_tags.append(CW_TAG)
-    
-    toot_str = ''
-    
-    if "blog" in entry['tags']:
-        toot_str += "New #Blog: "
-    elif "documentation" in entry['tags']:
-        toot_str += "New #Documentation: "
-    
-    toot_str += f"{entry['title']}\n"
-    
-    if entry['author']:
-        toot_str += f"Author: {entry['author']}\n"
-    
-    toot_str += f"\n\n{entry['link']}\n\n"
-    
-    # Tags to hashtags
-    if len(entry['tags']) > 0:
-        for tag in entry['tags']:
-            if tag in skip_tags:
-                # Skip the tag
-                continue
-            toot_str += f'#{tag.replace(" ", "")} '
-        
-    return toot_str
-
-
-def send_toot(en):
-    ''' Turn the dict into toot text
-    
-    and send the toot
-    '''
-    toot_txt = build_toot(en)
-    #print(toot_txt)
-    
-    headers = {
-        "Authorization" : f"Bearer {MASTODON_TOKEN}"
-        }
-    
-    data = {
-        'status': toot_txt,
-        'visibility': MASTODON_VISIBILITY
-        }
-    
-    if en['cw']:
-        data['spoiler_text'] = en['title']
-    
-    if DRY_RUN == "Y":
-        print("------")
-        print(data['status'])
-        print(data)
-        print("------")
-        return True
-    
-    try:
-        resp = SESSION.post(
-            f"{MASTODON_URL.strip('/')}/api/v1/statuses",
-            data=data,
-            headers=headers
-        )
-        
-        if resp.status_code == 200:
-            return True
-        else:
-            print(f"Failed to post {en['link']}")
-            print(resp.status_code)
-            return False
-    except:
-        print(f"Urg, exception {en['link']}")
-        return False
-    
-
-
 def check_if_link_seen(mode, linkhash, storedhash, feed):
     ''' Check whether the current hashed URL has previously been seen
     Return: boolean
@@ -157,10 +77,6 @@ def process_feed(feed):
             # Iterate over tags and add them
             [en['tags'].append(x['term']) for x in entry.tags]
         
-        en['cw'] = CW_TAG in en['tags']
-        
-        if INCLUDE_AUTHOR == "True" and hasattr(entry, "author"):
-            en['author'] = entry.author
         
         #print(en)
 
@@ -168,13 +84,11 @@ def process_feed(feed):
         # misc/Python_mastodon_rss_bot#1
         if not firsthash:
             firsthash = linkhash
-            
-        # Send the toot
-        if send_toot(en):
-            # If that worked, write hash to disk to prevent re-sending
-            write_hash_to_storage(TRACKING_MODE, linkhash, feed, hashtracker, firsthash)
-            time.sleep(1)
-        
+
+        # TODO: actually do something with it
+        print(f"seen {en}")
+        write_hash_to_storage(TRACKING_MODE, linkhash, feed, hashtracker, firsthash)
+
         # Increase the counter
         entry_count += 1
         
@@ -204,7 +118,7 @@ SESSION = requests.session()
 for feed in FEEDS:
     # Calculate the hashdir if not already set
     if "HASH_DIR" not in feed:
-        feed['HASH_DIR'] = f"{HASH_DIR}/{feed['FEED_URL'].replace("/","-").replace(".","-")}.urls"
+        feed['HASH_DIR'] = f"{HASH_DIR}/{feed['FEED_URL'].replace('/','-').replace('.','-')}.urls"
 
     if not os.path.exists(feed['HASH_DIR']):
         os.makedirs(feed['HASH_DIR'])
